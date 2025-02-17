41f96070ae3ddf3669303cd273e960b0df33d9ea
Amila Tharangani <amila89620@gmail.com>
2025-02-18 03:07:23 +0530

    Merge branch 'main' of https://github.com/amk1001928/Test1_PHP_LinnaTaxi

--- datafeedback.php
+++ datafeedback.php
@@ -2,11 +2,11 @@
 include 'db_connect_feedback.php'; // Include database connection
 
     // Escape user inputs to prevent SQL injection
-    $driver = $_POST["driver-Performance"];
-    $vehicle = $_POST["vehicle-condition"];
+    $driver = $_POST["driver_Performance"];
+    $vehicle = $_POST["vehicle_condition"];
     $timeline = $_POST["timeliness"];
-    $process = $_POST["booking-process"];
-    $overall= $_POST["overall-satisfaction"];
+    $process = $_POST["booking_process"];
+    $overall= $_POST["overall_satisfaction"];
 
     // SQL query to insert data
     $sql = "INSERT INTO feedback (driver-Performance, vehicle-condition, timeliness, booking-process,overall-satisfaction) 
