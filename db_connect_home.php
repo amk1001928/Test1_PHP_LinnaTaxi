9e815801dd5eaf2f3880024c299e68e1b38d0165
Darshana JA <amk1001928@student.hamk.fi>
2025-02-18 20:50:20 +0200

    db server changed

--- db_connect_home.php
+++ db_connect_home.php
@@ -5,7 +5,7 @@ ini_set('memory_limit', '1024M');
 // Assigning of server information
 $servername = "localhost";
 $username = "amk1001928";
-$password = "";
+$password = "0X5aE0uY";
 $dbname = "wp_amk1001928";
 
 // creating connection 
