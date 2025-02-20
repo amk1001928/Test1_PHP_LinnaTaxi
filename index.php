f9628078cbf6e651504696e65beed3b1bca5f2d1
Thanuja Manohari <thanuja53901@gmail.com>
2025-02-20 11:49:46 +0200

    add rel path for index path

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 
   <!-- Custom CSS -->
-  <link rel="stylesheet" href="index.css" />
+  <link rel="stylesheet" href="styles/index.css" />
 
   <!-- Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
