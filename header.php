5760def27d391cce2b12b57cd56371c3259f6a9c
Thanuja Manohari <thanuja53901@gmail.com>
2025-02-20 11:48:13 +0200

    Update header.php

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="header.css" />
+    <link rel="stylesheet" href="styles/header.css" />
     <title>Header - LinnaTaxi</title>
   </head>
 
